0d4e8209a1bae9718d286372c3715c77638f023a
Felix <61831466+FelixLie@users.noreply.github.com>
2021-01-25 16:08:56 +0100

    Datenbank Verbindung implementiert

---
+++ db.js
@@ -0,0 +1,24 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'SonnenstrahlAG',
+    database: 'test',
+});
+
+async function dbAbruf() {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        rows = await conn.query("SELECT * FROM dummy");
+        console.log(rows);
+        
+    } catch (err) {
+        throw err;
+
+    } finally{
+        if(conn) return conn.end();
+    }
+}
+
+module.exports = dbAbruf; 
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const dbAbruf = require('./db');
 
 const app = express();
+const datenbankabfrage = dbAbruf();
 
 app.use(express.static('public'));
 app.use(express.static('sources'));
 app.listen(3000);
+
+
