d2a8bfda719555a12f5f66b91229b6f6534077bb
juliansko <57212021+juliansko@users.noreply.github.com>
2021-02-01 21:07:50 +0100

    Get Route

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const provideDatabase = require('./db').createDatabase;
 const importCSV = require('./helper/importCSV').importDATA;
-const RestAPI = require('./rest') // noch nicht verwendet
+const connection = require("./db").getConnectionSonnenstrahl;
+//const groute = require('./rest').getroute // noch nicht verwendet
 
 
 
@@ -14,8 +15,16 @@ const database = provideDatabase();
 
 // CSV Import
 importCSV().then(() => console.log(`Import Done!`));
-
-
+//const gr = groute;
+//Get Route
+app.get("/rates", async (req, res) => {
+    const {zipCode = "%", consumption=500} = req.query
+    const conn = await connection();
+       rows = await conn.query("SELECT Tarifname, Fixkosten, plz, VariableKosten, Fixkosten + VariableKosten * ? AS monatliche_kosten FROM tarifdaten WHERE plz LIKE ?;",[consumption, zipCode]
+        );
+      res.send(rows);
+   if (conn) return conn.end();
+    });
 app.listen(3000);
 
 
