918beb0cbce41e7debbac2bd5b0a2c77f83c69ea
juliansko <57212021+juliansko@users.noreply.github.com>
2021-02-02 13:10:17 +0100

    Create rates.js

---
+++ public/rates.js
@@ -0,0 +1,19 @@
+const Sonnenstrahl = require("./db").getConnectionSonnenstrahl;
+
+const showTables = exports.showTables = async () => {
+    try{
+        let rows;
+        let ZipCode = document.getElementById('zip');
+        let consumption = document.getElementById('consump');
+        const conn = await Sonnenstrahl();
+        rows = conn.query('SELECT RateName, FixedCosts, ZipCode, VariableCosts, FixedCosts + VariableCosts * ? AS MonthlyCosts FROM RateData WHERE ZipCode LIKE ? AND Status = "active";',
+        [consumption, ZipCode],
+        (err, result) => {
+            if (err) throw err;
+            console.log(result);
+     });
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end;
+    }
